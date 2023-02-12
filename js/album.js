c46fa820f0b95a107f348ec8b158aa563671be09
Bojan3v <122092302+Bojan3v@users.noreply.github.com>
2023-02-12 16:08:13 +0300

    Add files via upload

--- js/album.js
+++ js/album.js
@@ -5,13 +5,7 @@ let search = new URLSearchParams(window.location.search);
 let i = search.get(`i`);
 let album = albums[i];
 
-if (!album) {
-  container.innerHTML = `Ошибка`;
-  setTimeout(() => {
-    window.location.pathname = `index.html`;
-  }, 5000);
-} else {
-  container.innerHTML = `
+container.innerHTML = `
 
     <div class="card mb-3">
         <div class="row cart">
@@ -33,20 +27,84 @@ if (!album) {
 
 `;
 
-  let tracks = album.tracks;
+let tracks = album.tracks;
 
-  for (let i = 0; i < tracks.length; i++) {
-    let track = tracks[i];
-    playlist.innerHTML += `
-    <li class="list-group-item d-flex align-items-center">
-        <img src="assets/290128_audio_media_music_play_player_icon.png" alt="" height="30px"
-            class="me-3">
+for (let i = 0; i < tracks.length; i++) {
+  let track = tracks[i];
+  playlist.innerHTML += `
+    <li class="list-group-item d-flex align-items-center track">
+        <img src="assets/290128_audio_media_music_play_player_icon.png" alt="" height="30px" class="me-3 img-pause">
+        <img src="assets/music_play.png" alt="" height="30px" class="me-3 d-none img-play">
         <div>
             <div>${track.title}</div>
             <div><smal class="text-muted">${track.author}</smal></div>
         </div>
-        <div class="ms-auto">${track.time}</div>
+        <div class="ms-auto time">${track.time}</div>
+        <audio class="audio" src="${track.src}"></audio>
     </li>
     `;
+}
+
+function setupAudio() {
+  // Найди коллекцию с треками
+  let trackNodes = document.querySelectorAll(`.track`);
+  let tracks = album.tracks;
+  for (let i = 0; i < trackNodes.length; i++) {
+    // Один элемент
+    let node = trackNodes[i];
+    let timeNode = node.querySelector(`.time`);
+    // Тег аудио внутри этого элемента
+    let audio = node.querySelector(`.audio`);
+
+    // продолжи самостоятельно
+    let track = tracks[i];
+    let imgPause = node.querySelector(`.img-pause`);
+    let imgPlay = node.querySelector(`.img-play`);
+    node.addEventListener(`click`, function () {
+      // Если трек сейчас играет...
+      if (track.isPlaying) {
+        track.isPlaying = false;
+        // Поставить на паузу
+        audio.pause();
+        imgPause.classList.remove(`d-none`);
+        imgPlay.classList.add(`d-none`);
+
+        // Если трек сейчас не играет...
+      } else {
+        track.isPlaying = true;
+        // Включить проигрывание
+        audio.play();
+        imgPause.classList.add(`d-none`);
+        imgPlay.classList.remove(`d-none`);
+        updateProgress()
+      }
+    });
+    function updateProgress() {
+      // Нарисовать актуальное время
+      let time = getTime(audio.currentTime)
+      if(time != timeNode.innerHTML){
+        timeNode.innerHTML = getTime(audio.currentTime);
+      }
+      
+
+      // Нужно ли вызвать её ещё раз?
+      if (track.isPlaying) {
+        requestAnimationFrame(updateProgress);
+      }
+    }
+    
   }
+}
+setupAudio();
+function getTime(time){
+  let currentSeconds = Math.floor(time);
+  let minutes = Math.floor(currentSeconds / 60);
+  let seconds = Math.floor(currentSeconds % 60);
+  if(minutes < 10){
+    minutes = `0` + minutes;
+  }
+  if(seconds < 10){
+    seconds = `0` + seconds;
+  }
+  return `${minutes}:${seconds}`
 }
--- js/data.js
+++ js/data.js
@@ -1,29 +1,37 @@
 let albums = [
     {
-      title: `Лучшее: музыка для чтения`,
-      img: `assets/200x200.jfif`,
-      description: `Музыка для чтения Фоновая музыка: самые популярные треки.`,
+      title: `Лучшее: Rammstein`,
+      img: `assets/Rammstein.jfif`,
+      description: `Rammstein: самые популярные треки`,
       year: 2023,
       tracks: [
         {
-            title: `Музыка (Настроение)`,
-            author: `Музыка для чтения`,
-            time: `2:06`
+            title: `Du Hast`,
+            author: `Rammstein`,
+            time: `4:12`,
+            src:`music/Rammstein_-_Du_hast_35843405.mp3`,
+            IsPlaing: false
         },
         {
-            title: `Музыка (Изучение)`,
-            author: `Музыка для чтения`,
-            time: `2:25`
+            title: `Adios`,
+            author: `Rammstein`,
+            time: `3:49`,
+            src:`music/Rammstein_-_Adios_35843402.mp3`,
+            IsPlaing: false
         },
         {
-            title: `Музыка (Чтение)`,
-            author: `Музыка для чтения`,
-            time: `2:10`
+            title: `Sonne`,
+            author: `Rammstein`,
+            time: `4:32`,
+            src:`music/Rammstein_-_Sonne_35843406.mp3`,
+            IsPlaing: false
         },
         {
-            title: `Впечатление (Книги)`,
-            author: `Музыка для чтения`,
-            time: `1:53`
+            title: `Ich will`,
+            author: `Rammstein`,
+            time: `3:37`,
+            src:`music/Rammstein_-_Ich_will_57658983.mp3`,
+            IsPlaing: false
         },
       ]
     },
@@ -36,22 +44,23 @@ let albums = [
         {
             title: `Symphony No. 38 in D-Sharp Major, .: III. Finale. Presto`,
             author: `Фольфганг Амадей Моцарт`,
-            time: `4:08`
+            time: `6:23`,
+            src:`music/wolfgang-amadeus-mozart-symphony-no-38-in-d-k-504-prague-3-finale-pres.mp3`,
+            IsPlaing: false
         },
         {
-            title: `Serenade for Winds in C-Sharp Minor, .: II. Andante`,
+            title: `Serenade No. 12 for wind Instruments in C Minor, .: II. Andante`,
             author: `Фольфганг Амадей Моцарт`,
-            time: `4:26`
-        },
-        {
-            title: `Serenade for Winds in C-Sharp Minor, .: IV. Allegro`,
-            author: `Фольфганг Амадей Моцарт`,
-            time: `5:59`
+            time: `4:39`,
+            src:`music/Newell_Jenkins_Everest_Woodwind_Octet_Volfgang_Amadejj_Mocart_-_Serenade_No_12_for_wind_Instruments_in_C_Minor_K_388_Nacht_Musique_II_Andante_48318771.mp3`,
+            IsPlaing: false
         },
         {
             title: `Serenade for Winds in C-Sharp Minor, .: III. Menuetto`,
             author: `Фольфганг Амадей Моцарт`,
-            time: `4:40`
+            time: `4:40`,
+            src:`music/Chamber Ensemble of Professors Conservatory - Serenade for Winds in C-Sharp Minor  III Menuetto.mp3`,
+            IsPlaing: false
         },
       ]
     },
@@ -64,22 +73,30 @@ let albums = [
         {
             title: ` Silfur-Refur`,
             author: `Sólstafir`,
-            time: `6:54`
+            time: `6:54`,
+            src:`music/Slstafir_-_Silfur-Refur_47876361.mp3`,
+            IsPlaing: false
         },
         {
             title: `Ísafold`,
             author: `Sólstafir`,
-            time: `4:58`
+            time: `4:58`,
+            src:`music/Slstafir_-_safold_47876363.mp3`,
+            IsPlaing: false
         },
         {
             title: `Heuristic`,
             author: `The Enos`,
-            time: `4:06`
+            time: `4:06`,
+            src:`music/The_Enos_-_Heuristic_70897608.mp3`,
+            IsPlaing: false
         },
         {
             title: `Группа крови`,
             author: `КИНО`,
-            time: `4:43`
+            time: `4:43`,
+            src:`music/Kino_-_Gruppa_krovi_47829944.mp3`,
+            IsPlaing: false
         },
       ]
     },
@@ -90,24 +107,32 @@ let albums = [
       year: 2023,
       tracks: [
         {
-            title: `Vanita`,
+            title: `Vanity`,
             author: `Mica Levi`,
-            time: `2:59`
+            time: `2:59`,
+            src:`music/Mica_Levi_-_Vanity_72896792.mp3`,
+            IsPlaing: false
         },
         {
             title: `We Move Lightl`,
             author: `Dustin O'Halloran`,
-            time: `3:10`
+            time: `3:10`,
+            src:`music/Dustin_OHalloran_-_We_Move_Lightly_48114812.mp3`,
+            IsPlaing: false
         },
         {
             title: `It’s Over / Engagement Party`,
             author: `Justin Hurwitz`,
-            time: `1:34`
+            time: `1:34`,
+            src:`music/Justin_Hurwitz_-_Its_Over_Engagement_Party_48079969.mp3`,
+            IsPlaing: false
         },
         {
             title: `Pour Les Oiseaux`,
             author: `Sylvain Chauveau`,
-            time: `1:16`
+            time: `1:16`,
+            src:`music/Sylvain_Chauveau_-_Pour_Les_Oiseaux_48115248.mp3`,
+            IsPlaing: false
         },
       ]
     }
